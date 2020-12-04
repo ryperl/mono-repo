189c8796944c4c32b4dd0e0af8b591ae2025f48c
Ryan Perlman <ryperl@microsoft.com>
2020-12-03 16:04:53 -0800

    change

--- packages/common/index.js
+++ packages/common/index.js
@@ -2,4 +2,5 @@ module.exports = () => {
     console.log('hello from common 12');
     console.log('hello from common 13');
     console.log('hello from common 14');
+    console.log('hello 15');
 }
